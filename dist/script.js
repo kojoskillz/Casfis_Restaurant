aec2823b0b4e6c0c0ae52aed007468be86837b68
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-10-03 00:22:34 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,48 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
+// animate_on_scroll
+const typed = new Typed('#element', {
+  strings: ['Front-End Developer', 'Technical Writer'],
+  typeSpeed: 90,
+  backSpeed:20,
+  loop:true,
+  
+  
+
+});
+
+
+// move_to_top
+
+
+function moveTop() {
+  window.scrollTo(0,0);
+}
+
+// nav_bar
+
+
+
+
+
